f27959f5e053a23d154fb4c0d75359210017ff43
Dwayne Hart <dwayne.hart@gmail.com>
2020-09-22 08:50:40 -0230

    Restructured some of the logic around the updating of a person's .bashrc file.

--- install-oceannavigator.sh
+++ install-oceannavigator.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-printf "Where do you wish to install the Ocean Navigator software?\n\n" 
+printf "\nWhere do you wish to install the Ocean Navigator software?\n\n" 
+printf "ONAV-installer> "
 read LOCATION
 
 printf "\nInstalling into.... %s\n\n" ${LOCATION}
@@ -22,7 +23,7 @@ fi
 
 if [ ! -d ${LOCATION}/tools/miniconda/3/amd64 ] ; then
    mkdir ${LOCATION}/tools
-   printf "\nInstalling the Miniconda software to %s.\n\n" ${LOCATION}/tools/miniconda/3/amd64
+   printf "Installing the Miniconda software to %s.\n\n" ${LOCATION}/tools/miniconda/3/amd64
    bash ${LOCATION}/.tmp/Miniconda3-latest-Linux-x86_64.sh -b -p ${LOCATION}/tools/miniconda/3/amd64
 else
    printf "\nAn instance of the Ocean Navigator exists. Exiting!\n"
@@ -59,9 +60,9 @@ else
    printf "unset __conda_setup\n" >> ${LOCATION}/conf/onav-env.sh
    printf "# <<< conda initialize <<<\n\n" >> ${LOCATION}/conf/onav-env.sh
    printf "conda activate navigator\n\n" >> ${LOCATION}/conf/onav-env.sh
-   printf "export NVM_DIR=\"LOCATION/tools/nvm\"\n" >> ${LOCATION}/conf/onav-env.sh
-   printf "[ -s \"\$NVM_DIR/nvm.sh\" ] && \\. \"\$NVM_DIR/nvm.sh\"  # This loads nvm\n" >> ${LOCATION}/conf/onav-env.sh
-   printf "[ -s \"\$NVM_DIR/bash_completion\" ] && \\. \"\$NVM_DIR/bash_completion\"  # This loads nvm bash_completion\n" >> ${LOCATION}/conf/onav-env.sh
+   printf "export NVM_DIR=\"${LOCATION}/tools/nvm\"\n" >> ${LOCATION}/conf/onav-env.sh
+   printf '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"\n' >> ${LOCATION}/conf/onav-env.sh
+   printf '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"\n' >> ${LOCATION}/conf/onav-env.sh
    printf "\n" >> ${LOCATION}/conf/onav-env.sh
    echo "export PATH=${LOCATION}/tools/yarn/bin:\$PATH" >> ${LOCATION}/conf/onav-env.sh
    echo "export PATH=${LOCATION}/tools/bower/bin:\$PATH" >> ${LOCATION}/conf/onav-env.sh
@@ -75,29 +76,25 @@ else
    printf "\nDownloading the Ocean-Data-Map-Project.git repo and placing it in %s.\n" ${LOCATION}/Ocean-Data-Map-Project
    mkdir ${LOCATION}/Ocean-Data-Map-Project
    git clone https://github.com/DFO-Ocean-Navigator/Ocean-Data-Map-Project.git ${LOCATION}/Ocean-Data-Map-Project
-   
-   printf "Bringing in a site specific configuration file for Dorval.\n"
-   cd ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/configs
-   git checkout dorval
 
-   printf "Creating a cache directory.\n"
+   printf "\nCreating a cache directory.\n"
    mkdir -p ${LOCATION}/cache/oceannavigator
 
-   printf "Adjusting the %s configuration file.\n" ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/configs/oceannavigator.cfg
-   git clone https://github.com/DFO-Ocean-Navigator/dorval.git ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/configs
-   cd ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/configs
-   git checkout dorval
-   cd -
-   sed -i "s/LOCATION/${LOCATION}/" ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/configs/oceannavigator.cfg
+   printf "\nBringing in a site specific configuration file for Dorval.\n"
+   rm -rf ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/configs 
+   git clone https://github.com/DFO-Ocean-Navigator/dorval.git ${LOCATION}/.tmp/dorval
+   mv ${LOCATION}/.tmp/dorval/configs ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/configs
+   sed -i 's/LOCATION/${LOCATION}/' ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/configs/oceannavigator.cfg
 
-   printf "In order to configure your account to work with conda we need to know what\n"
+   printf "\nIn order to configure your account to work with conda we need to know what\n"
    printf "shell you are using. Your choices are bash, fish, tcsh, xonsh, zsh, or\n"
-   printf " powershell.\n"
+   printf "powershell.\n"
+   printf "\nONAV-installer> "
    read SHELL_NAME
-   printf "\n\n"
+   printf "\n"
    conda init ${SHELL_NAME}
 
-   printf "\n\nInstalling aand configuring the CONDA Python environments.\n"
+   printf "\nInstalling and configuring the CONDA Python environments.\n"
    conda create --quiet --name navigator --file ${LOCATION}/Ocean-Data-Map-Project/config/conda/navigator-spec-file.txt
    conda create --quiet --name index-tool --file ${LOCATION}/Ocean-Data-Map-Project/config/conda/index-tool-spec-file.txt
    conda create --quiet --name nco-tools --file ${LOCATION}/Ocean-Data-Map-Project/config/conda/nco-tools-spec-file.txt
@@ -105,10 +102,11 @@ else
    printf "\nDownloading the NVM software in order to install Node.\n"
    git clone https://github.com/nvm-sh/nvm.git ${LOCATION}/tools/nvm
    printf "export NVM_DIR=\"${LOCATION}/tools/nvm\"\n" >> ${HOME}/.bashrc
-   printf "[ -s "$NVM_DIR/nvm.sh" ] && \. \"$NVM_DIR/nvm.sh\"" >> ${HOME}.bashrc 
-   printf "[ -s "$NVM_DIR/bash_completion" ] && \. \"$NVM_DIR/bash_completion\"" >> ${HOME}/.bashrc
-   source .bashrc
-   bash -c "nvm install v8.16.0"
+   printf '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"\n' >> ${HOME}/.bashrc 
+   printf '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"\n' >> ${HOME}/.bashrc
+   printf "export PATH=${LOCATION}/tools/bower/bin:${PATH}" >> ${HOME}/.bashrc
+   printf "export PATH=${LOCATION}/tools/bower/bin:${PATH}">> ${HOME}/.bashrc
+   bash -c "source ${HOME}/.bashrc ; nvm install v8.16.0"
 
    printf "\nDownloading YARN and unbundling the tarball and moving it to %s.\n" ${LOCATION}/tools/yarn
    wget --quiet -O ${LOCATION}/.tmp/yarn-v1.22.5.tar.gz http://navigator.oceansdata.ca/yarn/yarn-v1.22.5.tar.gz
@@ -117,13 +115,12 @@ else
    export PATH=${LOCATION}/tools/yarn/bin:${PATH}
 
    printf "\nDownloading and installing BOWER.\n"
-   yarn global add bower --prefix ${LOCATION}/tools/bower --global-folder 
+   bash -c "source ${HOME}/.bashrc ; yarn global add bower --prefix ${LOCATION}/tools/bower"
    export PATH=${LOCATION}/tools/bower/bin:${PATH}
 
    printf "\nCompiling the Javascript frontend of the Ocean Navigator.\n"
    cd ${LOCATION}/Ocean-Data-Map-Project/oceannavigator/frontend
-   yarn install
-   yarn build
+   bash -c "source ${HOME}/.bashrc ; yarn install ; yarn build"
    cd -
 
    printf "\nDownloading the indexing toolchain to %s.\n" ${LOCATION}/netcdf-timestamp-mapper
@@ -131,15 +128,12 @@ else
    git clone https://github.com/DFO-Ocean-Navigator/netcdf-timestamp-mapper.git ${LOCATION}/netcdf-timestamp-mapper
    cd ${LOCATION}/netcdf-timestamp-mapper
    git submodule update --init --recursive
+   bash -c "source ${HOME}/.bashrc ; conda activate index-tool ; make"
+   bash -c "source ${HOME}/.bashrc ; conda activate navigator ; pip install defopt ; pip install visvalingamwyatt"
 
-   printf "\nNow that the installation has completed. You now need to source the\n"
    printf "Ocean Navigator environment configuration file by running the command...\n"
    printf "\tsource ${LOCATION}/conf/onav-env.sh\n" 
-   printf "\nThe following pip files are needed.\n"
-   printf "\tpip install defopt && pip install visvalingamwyatt\n"
-   printf "\n\nAs well, the indexing tool needs to be built. Please use the following\n"
-   printf "commands to compile the indexing toolchain.\n"
-   printf "\tcd ${LOCATION}/netcdf-timestamp-mapper\n"
-   printf "\tconda activate index-tool && make && conda deactivate\n"
-
+   printf "Change your working directory to ${LOCATION}/Ocean-Data-Map-Project and launch\n"
+   printf "the Ocean Navigator web services by issuing the following command;\n"
+   printf "\t./launch-web-service.sh\n"
 fi
